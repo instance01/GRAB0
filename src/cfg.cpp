1c704f11661944a2796bcfaa5f037f76e04e6c8c
instance01 <nibbadont@protonmail.com>
2020-09-17 08:28:56 +0200

    Add experimental settings to cfg.

--- src/cfg.cpp
+++ src/cfg.cpp
@@ -56,6 +56,7 @@ json get_default(std::string base) {
       {"use_eps_greedy_learning", true},
       {"eps_greedy_epsilon_decay_factor_train", 0.995},
       {"eps_greedy_epsilon_decay_factor_actor", 0.995},
+      {"eps_greedy_epsilon_decay_factor_follow_a2c", 0.995},
       {"grad_bandit_init_random", true},
       {"grad_bandit_reward_power", 1},
       {"grad_bandit_tau_schedule_tau", {20, 90, 80}},
@@ -119,6 +120,7 @@ json get_default(std::string base) {
       {"use_eps_greedy_learning", true},
       {"eps_greedy_epsilon_decay_factor_train", 0.995},
       {"eps_greedy_epsilon_decay_factor_actor", 0.995},
+      {"eps_greedy_epsilon_decay_factor_follow_a2c", 0.995},
       {"grad_bandit_init_random", true},
       {"grad_bandit_reward_power", 1},
       {"grad_bandit_tau_schedule_tau", {20, 90, 80}},
@@ -177,6 +179,7 @@ json get_default(std::string base) {
       {"use_eps_greedy_learning", true},
       {"eps_greedy_epsilon_decay_factor_train", 0.995},
       {"eps_greedy_epsilon_decay_factor_actor", 0.995},
+      {"eps_greedy_epsilon_decay_factor_follow_a2c", 0.995},
       {"grad_bandit_init_random", true},
       {"grad_bandit_reward_power", 1},
       {"grad_bandit_tau_schedule_tau", {20, 90, 80}},
@@ -235,6 +238,7 @@ json get_default(std::string base) {
       {"use_eps_greedy_learning", true},
       {"eps_greedy_epsilon_decay_factor_train", 0.99},
       {"eps_greedy_epsilon_decay_factor_actor", 0.99},
+      {"eps_greedy_epsilon_decay_factor_follow_a2c", 0.99},
       {"grad_bandit_init_random", true},
       {"grad_bandit_reward_power", 1},
       {"grad_bandit_tau_schedule_tau", {20, 90, 80}},
@@ -244,6 +248,72 @@ json get_default(std::string base) {
       {"follow_a2c", false},
       {"eval_break_on_good_avg_reward", false},
       {"eval_min_avg_reward", -110},
+
+      // Other
+      {"reward_exponent", 1},
+    }},
+    {"rip1", {
+      {"n_actions", 3},
+      {"n_input_features", 4},
+      {"n_runs", 5},
+
+      // MCTS
+      {"gamma", .99},
+      {"simulations", 1500},
+      {"horizon", 200},
+      {"dirichlet_alpha", .3},
+      {"dirichlet_frac", .1},
+      {"pb_c_base", 19000},
+      {"pb_c_init", .3},
+      {"tough_ce", false},
+
+      // A2C
+      {"alpha", .00001},
+      {"net_architecture", {10, 5}},
+      {"schedule_alpha", false},
+      {"scheduler_class", "exp"},  // exp, step, reduce_eval
+      {"scheduler_factor", .995},  // For: exp, step, reduce_eval
+      {"scheduler_min_lr", .000001},  // For: exp, step, reduce_eval
+      {"scheduler_steps", {100, 200}},  // For: step
+      {"scheduler_min_good_eval", -120.0},  // For: reduce_eval
+      {"scheduler_min_n_good_evals", 20}, // For: reduce_eval
+      {"scheduler_consecutive", true},  // For: reduce_eval
+      {"use_weight_decay", false},
+      {"weight_decay", .00001},
+      {"optimizer_class", "adam"},  // adam, sgd
+      {"sgd_momentum", .9},
+
+      // AlphaZero
+      {"memory_capacity", 100000},
+      {"prioritized_sampling", false},
+      {"episodes", 1500},
+      {"n_procs", 8},
+      {"n_actors", 25},  // 5000
+      {"train_steps", 50},  // 700000
+      {"desired_eval_len", 0},
+      {"n_desired_eval_len", 100},
+      {"bandit_type", "grad"},  // mcts, grad
+      {"grad_bandit_alpha", 0.015},
+      {"use_eps_greedy_learning", true},
+      {"eps_greedy_epsilon_decay_factor_train", 0.9994},
+      {"eps_greedy_epsilon_decay_factor_actor", 0.998},
+      {"eps_greedy_epsilon_decay_factor_follow_a2c", 0.998},
+      {"grad_bandit_init_random", true},
+      {"grad_bandit_reward_power", 1},
+      {"grad_bandit_tau_schedule_tau", {20, 90, 80}},
+      {"grad_bandit_tau_schedule_k", {0, 1500, 2000}},
+      {"do_warmup", true},
+      {"n_warmup", 50},
+      {"follow_a2c", true},
+      {"eval_break_on_good_avg_reward", true},
+      {"eval_min_avg_reward", 195},
+
+      {"experimental_top_fill", false},
+      {"experimental_follow_after_ep", 300},
+      {"experimental_follow_eps_power", 2},
+      {"experimental_follow_after_ep2", 900},
+      {"experimental_follow_eps_power2", 2},
+      {"experimental_top_cutoff", 195},
 
       // Other
       {"reward_exponent", 1},
