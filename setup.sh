96aca670e65c28929f6d5afe7b62e7623ebef288
instance01 <nibbadont@protonmail.com>
2020-11-06 21:26:23 +0100

    Add box2d to setup.sh.

--- setup.sh
+++ setup.sh
@@ -3,14 +3,22 @@ mkdir dist
 cd dist
 
 # Setup dependencies.
+# libtorch
 # In case an error happens here, mirror: https://mega.nz/file/tt9WDYDK#DVEtQ99AhtnhLmOQ3G4Ogc_HFgNoY6Dn4Ji9_qyw69E
 wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.5.0%2Bcpu.zip && unzip "libtorch-cxx11-abi-shared-with-deps-1.5.0+cpu.zip"
+# nlohmann-json
 git clone https://github.com/nlohmann/json.git
 cd json
 cmake .
+# box2d
+cd ..
+git clone --branch instance https://github.com/Instance-contrib/box2d.git
+cd box2d
+./build.sh
+cp ./src/box2dConfigVersion.cmake ./build/src/box2dConfig.cmake
+cd ..
 
 # Build the project.
-cd ../..
 cmake .
 cmake --build .
 
